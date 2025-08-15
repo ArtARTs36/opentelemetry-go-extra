8c0f31084fed05983ca08b7281dcc185d0d36ad1
artarts36 <temicska99@mail.ru>
2025-08-15 22:55:10 +0300

    feat: trace marshalled rows count

---
+++ otelsql/rows.go
@@ -0,0 +1,39 @@
+package otelsql
+
+import (
+	"database/sql/driver"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type countableRows struct {
+	rows driver.Rows
+	span trace.Span
+
+	count int
+}
+
+func newCountableRows(
+	rows driver.Rows,
+	span trace.Span,
+) *countableRows {
+	return &countableRows{rows: rows, span: span}
+}
+
+func (c *countableRows) Columns() []string {
+	return c.rows.Columns()
+}
+
+func (c *countableRows) Close() error {
+	c.span.SetAttributes(dbRowsMarshalled.Int(c.count))
+	c.span.End()
+
+	return c.rows.Close()
+}
+
+func (c *countableRows) Next(dest []driver.Value) error {
+	err := c.rows.Next(dest)
+	if err != nil {
+		c.count++
+	}
+	return err
+}
